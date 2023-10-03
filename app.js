b52e58b5af81d042c616a34ad2ecb3a7d2496e70
Hans <dev.e@sylents.de>
2023-10-03 15:41:11 +0200

    production distribution fix

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
+import Config from './config.js';
+
+
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("solarForm");
 
     form.addEventListener("submit", function (e) {
         e.preventDefault();
-        const kW = parseFloat(document.getElementById("kW").value);
 
-        // Updated parameters
-        const kW_per_panel = 0.4;
-        const m2_per_kW = 2 / kW_per_panel;
-        const cost_per_kW = 2500;
-        const no_of_panels_per_kW = 1 / kW_per_panel;
-        const kWh_per_year_per_kW = 1000;
+        const kWp = parseFloat(form.elements.kW.value);
 
-        const m2 = m2_per_kW * kW;
-        const cost = cost_per_kW * kW;
-        let no_of_panels = no_of_panels_per_kW * kW;
+        const m2 = Config.m2_per_kW * kWp;
+        const cost = Config.cost_per_kW * kWp;
+        let no_of_panels = Config.no_of_panels_per_kW * kWp;
         no_of_panels = Math.ceil(no_of_panels);
-        const kWh_per_year = Math.ceil(kWh_per_year_per_kW * kW);
+        const kWh_per_year = Math.ceil(Config.kWh_per_year_per_kW * kWp);
+
 
         const resultHTML = `
       <h2>Results:</h2>
@@ -28,12 +26,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
         document.getElementById("results").innerHTML = resultHTML;
 
-        // Monthly distribution (sample for 10,000 kWh)
-        const monthlyDist = [205, 316, 803, 1385, 1460, 1393, 1460, 1240, 926, 410, 265, 137];
+        // Monthly distribution (kWh Energy per Day per kWp installed)
+        const monthlyDist = [0.9, 1.5, 3.1, 4.8, 4.5, 4.0, 4.3, 3.9, 3.4, 1.5, 1.2, 0.5];
 
         // Daily average kWh production per month
         const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-        const dailyAvg = monthlyDist.map((kWh, index) => (kWh / daysInMonth[index]).toFixed(2));
+        const actualDailyAvg = monthlyDist.map(kWh_per_day_per_kWp => kWh_per_day_per_kWp * kWp);
 
         // Central Europe consumption
         const singlePersonMonthly = [150, 130, 120, 110, 100, 90, 90, 100, 110, 120, 140, 150];
@@ -49,7 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 datasets: [
                     {
                         label: 'Daily Avg Production kWh',
-                        data: dailyAvg,
+                        data: actualDailyAvg,
                         backgroundColor: 'rgba(75, 192, 192, 0.5)',
                         borderColor: 'rgba(75, 192, 192, 1)',
                         borderWidth: 1
---
+++ config.js
@@ -0,0 +1,11 @@
+// This is the config file that stores all the parameters
+
+const Config = {
+    kW_per_panel: 0.4,
+    m2_per_kW: 2 / 0.4,
+    cost_per_kW: 2500,
+    no_of_panels_per_kW: 1 / 0.4,
+    kWh_per_year_per_kW: 1025
+};
+
+export default Config;
